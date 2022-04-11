15bc83987bd70fe456e2239d57b11f429b149d55
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2022-04-11 20:23:35 -0300

    knapsack implementation

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from cgitb import html
+from webbrowser import get
 from flask import Flask, render_template, request, flash, redirect, url_for
+import random
+
+
+def knapSack(W, wt, val, n):
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1]
+                              + K[i-1][w-wt[i-1]],
+                              K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+
+    return K[n][W]
+
 
 adj_list = {
     "1": ["2", "7", "8"],
@@ -16,7 +35,7 @@ adj_list = {
 }
 
 app = Flask(__name__)  # sempre ao iniciar um site
-
+app.config['SECRET_KEY'] = "minha-palavra-secreta"
 
 # criar a primeira página do site
 # route -> caminho que vem depois do dominio
@@ -45,19 +64,40 @@ def homepage():
 @app.route('/caminho/<no_destino>', methods=['GET', 'POST'])
 def caminho(no_destino):
     no = no_destino
+    val = [1, 6, 18, 22, 26]
+    wt = [1, 2, 5, 6, 7]
+    W = 11
+    n = len(val)
+    resposta = knapSack(W, wt, val, n)
 
     if request.method == "GET":
-        return render_template("caminho.html", no=no)
+        return render_template("caminho.html", no=no,
+                               valor0=val[0], peso0=wt[0],
+                               valor1=val[1], peso1=wt[1],
+                               valor2=val[2], peso2=wt[2],
+                               valor3=val[3], peso3=wt[3],
+                               valor4=val[4], peso4=wt[4],
+                               resposta=resposta, peso=W
+                               )
 
     else:
-        palpite = str(request.form.get("name"))
-        if (palpite == "10" and palpite in adj_list[no]):
-            return redirect(url_for('vitoria'))
-        if (palpite in adj_list[no]):
-            no = palpite
-            return redirect(url_for('caminho', no_destino=no))
-        else:
-            return redirect(url_for('errou'))
+        try:
+            valor = int(request.form.get("valor"))
+            if(valor == resposta):
+                flash("Resposta Certa!", "success")
+                return redirect(url_for('caminho', no_destino=no))
+            if(valor != resposta):
+                flash("Resposta errada!", "warning")
+                return redirect(url_for('caminho', no_destino=no))
+        except:
+            palpite = str(request.form.get("name"))
+            if (palpite == "10" and palpite in adj_list[no]):
+                return redirect(url_for('vitoria'))
+            if (palpite in adj_list[no]):
+                no = palpite
+                return redirect(url_for('caminho', no_destino=no))
+            else:
+                return redirect(url_for('errou'))
 
 
 @app.route('/errou', methods=['GET', 'POST'])
@@ -68,6 +108,11 @@ def errou():
 @app.route('/vitoria', methods=['GET', 'POST'])
 def vitoria():
     return render_template("vitoria.html")
+
+
+@app.route('/modal')
+def modal():
+    return render_template('modal.html',)
 
 
 # colocar o site no ar
